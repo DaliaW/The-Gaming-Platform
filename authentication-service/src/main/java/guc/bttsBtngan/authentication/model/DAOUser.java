1bc8aca87b7e89cc4df0fba5525b4fb5f0769ef2
Mazen Ashraf <mazen.ashraf.12@gmail.com>
2022-04-15 04:08:07 +0200

    added the DAO user model

---
+++ authentication-service/src/main/java/guc/bttsBtngan/authentication/model/DAOUser.java
@@ -0,0 +1,36 @@
+package guc.bttsBtngan.authentication.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "appUser")
+public class DAOUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column
+    private String username;
+
+    @Column
+    @JsonIgnore
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
