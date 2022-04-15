601901aa0b0761cb083833de45dda3977916e8c0
Mazen Ashraf <mazen.ashraf.12@gmail.com>
2022-04-15 04:08:46 +0200

    added the object user model

---
+++ authentication-service/src/main/java/guc/bttsBtngan/authentication/model/UserDTO.java
@@ -0,0 +1,22 @@
+package guc.bttsBtngan.authentication.model;
+
+public class UserDTO {
+    private String username;
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
