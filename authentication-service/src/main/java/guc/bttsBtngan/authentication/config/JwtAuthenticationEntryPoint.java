93873f7252f4aec9950106dc8248de7bc30cfe24
Mazen Ashraf <mazen.ashraf.12@gmail.com>
2022-04-15 06:00:30 +0200

    adding the authentication entry point

---
+++ authentication-service/src/main/java/guc/bttsBtngan/authentication/config/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,18 @@
+package guc.bttsBtngan.authentication.config;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Serializable;
+
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint, Serializable {
+    private static final long serialVersionUID = -7858869558953243875L;
+
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+         httpServletResponse.sendError(HttpServletResponse.SC_UNAUTHORIZED,"You are unauthorized!");
+    }
+}
