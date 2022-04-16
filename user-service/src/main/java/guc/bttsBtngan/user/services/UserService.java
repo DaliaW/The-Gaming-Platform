d7954e2026ec6acf3273b861d706edc42f9cca8c
Dalia Walid <47950134+DaliaW@users.noreply.github.com>
2022-04-16 05:21:18 +0200

    connected to mongo & postgres successfully

--- user-service/src/main/java/guc/bttsBtngan/user/services/UserService.java
+++ user-service/src/main/java/guc/bttsBtngan/user/services/UserService.java
@@ -1,6 +1,7 @@
 package guc.bttsBtngan.user.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,6 +14,9 @@ public class UserService {
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
+
+    @Autowired
+    MongoOperations mongoOperations;
 
     @GetMapping
     public String getAllUsers() {
