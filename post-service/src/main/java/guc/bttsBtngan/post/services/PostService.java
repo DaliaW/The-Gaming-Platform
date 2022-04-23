7e546739301cd9873f29278e93df7fe307472c81
Omar Alaskndrani <omar.alaskndrani.1@gmail.com>
2022-04-23 17:43:07 +0200

    follow post functionality implemented (not tested)

--- post-service/src/main/java/guc/bttsBtngan/post/services/PostService.java
+++ post-service/src/main/java/guc/bttsBtngan/post/services/PostService.java
@@ -5,9 +5,12 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+
 import guc.bttsBtngan.post.data.Post;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoOperations;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 
@@ -20,5 +23,18 @@ public class PostService {
     public String createPost(Post post) throws InterruptedException, ExecutionException {
         mongoOperations.save(post);
         return "DONE, created post is: "+(post).toString();
+    }
+    
+    public String followPost(String userId, String postId)throws InterruptedException, ExecutionException {
+    	Query query = new Query();
+    	query.addCriteria(Criteria.where("_id").is(postId));
+    	Post post = mongoOperations.findById(query, Post.class, "post");  	
+    			
+    	post.getPostFollowers().add(userId);
+    	
+    	mongoOperations.save(post);
+    	
+    	return "DONE, Potatoes follow post : "+(post).toString();
+    	
     }
 }
