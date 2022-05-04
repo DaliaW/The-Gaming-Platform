fcaa5e76bfbd353e23b2643eedac77de35844165
ElShabrawy99 <46649627+ElShabrawy99@users.noreply.github.com>
2022-05-04 17:38:26 +0200

    getDone

--- notification-ms/src/main/java/guc/bttsBtngan/notification/controller/NotificationController.java
+++ notification-ms/src/main/java/guc/bttsBtngan/notification/controller/NotificationController.java
@@ -3,11 +3,9 @@ package guc.bttsBtngan.notification.controller;
 import guc.bttsBtngan.notification.entity.Notifications;
 import guc.bttsBtngan.notification.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
 @RestController
@@ -19,7 +17,20 @@ public class NotificationController {
 
     @PostMapping("/notifications")
     public String saveNotification(@RequestBody Notifications notification)throws ExecutionException {
-        System.out.println("\n\nhi1\n\n");
-        return notificationService.saveNotification(notification);
+      //  System.out.println("\n\nhi1\n\n");
+        return notificationService.createNotification(notification);
+    }
+
+//    @GetMapping("/notifications/{name}")
+//    public Notifications getNotification(@PathVariable String name) throws ExecutionException, InterruptedException {
+//        //  System.out.println("\n\nhi1\n\n");
+//        return notificationService.getNotification(name);
+//    }
+
+
+    @GetMapping("/notifications/{userID}")
+    public ArrayList getNotification(@PathVariable String userID) throws ExecutionException, InterruptedException {
+        //  System.out.println("\n\nhi1\n\n");
+        return notificationService.getNotification(userID);
     }
 }
--- notification-ms/src/main/java/guc/bttsBtngan/notification/service/NotificationService.java
+++ notification-ms/src/main/java/guc/bttsBtngan/notification/service/NotificationService.java
@@ -1,18 +1,20 @@
 package guc.bttsBtngan.notification.service;
 
 import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.DocumentSnapshot;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.cloud.FirestoreClient;
 import guc.bttsBtngan.notification.entity.Notifications;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 @Service
-public class NotificationService {
-    public String saveNotification(Notifications notification){
+public class NotificationService<retun> {
+    public String createNotification(Notifications notification){
 
         Firestore firestoredb= FirestoreClient.getFirestore();
 
@@ -29,5 +31,59 @@ public class NotificationService {
         return null;
     }
 
-//    public String getNotification()
+//    public Notifications getNotification(String name) throws ExecutionException, InterruptedException {
+//        Firestore firestoredb= FirestoreClient.getFirestore();
+//
+//        DocumentReference documentReference= firestoredb.collection("notification").document(name);
+//
+//        ApiFuture<DocumentSnapshot> future=documentReference.get();
+//
+//        DocumentSnapshot document=future.get();
+//
+//        Notifications notifications=null;
+//        if(document.exists()){
+//            notifications=document.toObject(Notifications.class);
+//            return notifications;
+//        }else{
+//            return null;
+//        }
+//    }
+
+        public ArrayList<String> getNotification(String userID) throws ExecutionException, InterruptedException {
+            Firestore firestoredb = FirestoreClient.getFirestore();
+            ArrayList<String>notificationTypes=new ArrayList<String>();
+            Iterable<DocumentReference> documentReference = firestoredb.collection("notification").listDocuments();
+            Iterator<DocumentReference> iterator=documentReference.iterator();
+            while (iterator.hasNext()) {
+                ApiFuture<DocumentSnapshot> future = iterator.next().get();
+//                System.out.println(documentReference.iterator().next().get());
+//                System.out.println(documentReference);
+//                System.out.println("\nhiiiiiiiiiiiiiiiiiiiii\n");
+
+                DocumentSnapshot document = future.get();
+
+                Notifications notifications = null;
+                if (document.exists()) {
+                    notifications = document.toObject(Notifications.class);
+               //     System.out.println(document);
+                  //  System.out.println(notifications.getUserIDs().contains(userID));
+                    if(notifications.getUserIDs()!=null&&contains(notifications.getUserIDs(),userID)){
+                        System.out.println(notifications.getUserIDs().get(0));
+                        notificationTypes.add(notifications.getType());
+                    }
+                }
+              //  documentReference.iterator().next();
+            }
+            return notificationTypes;
+        }
+
+    private boolean contains(List<String> userIDs, String userID) {
+        System.out.println(userID);
+        for (int i=0;i<userIDs.size();i++){
+            if(userID.equals(userIDs.get(i).toString())){
+                return true;
+            }
+        }
+        return false;
+    }
 }
