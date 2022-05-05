f2df35fd8eabdd1cdd15ba6321ab92a057a2e664
Mazen Ashraf <mazen.ashraf.12@gmail.com>
2022-05-05 17:41:20 +0200

    bug fix

--- authentication-service/src/main/java/guc/bttsBtngan/authentication/dao/UserRepository.java
+++ authentication-service/src/main/java/guc/bttsBtngan/authentication/dao/UserRepository.java
@@ -3,7 +3,7 @@ package guc.bttsBtngan.authentication.dao;
 import guc.bttsBtngan.authentication.model.DAOUser;
 import org.springframework.data.repository.CrudRepository;
 
-public interface UserRespository extends CrudRepository<DAOUser,Long> {
+public interface UserRepository extends CrudRepository<DAOUser,Long> {
 
     DAOUser findByUsername(String username);
 
