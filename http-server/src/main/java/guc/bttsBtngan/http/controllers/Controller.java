073dd05e512c091eec6117db43bde904cd7a422f
Abdelaziz <abdelazizadel99@gmail.com>
2022-05-06 02:07:47 +0200

    added authentication check in http server

--- http-server/src/main/java/guc/bttsBtngan/http/controllers/Controller.java
+++ http-server/src/main/java/guc/bttsBtngan/http/controllers/Controller.java
@@ -25,7 +25,7 @@ public class Controller {
 		this.amqpTemplate = amqpTemplate;
 		Map<String, String> serviceToCommand = new HashMap<>();
 		serviceToCommand.put("chat", "messaging_req");
-		serviceToCommand.put("authentication", "messaging_req");
+		serviceToCommand.put("authentication", "authentication_req");
 		serviceToCommand.put("notification", "messaging_req");
 		serviceToCommand.put("user", "messaging_req");
 		serviceToCommand.put("post", "messaging_req");
@@ -40,24 +40,41 @@ public class Controller {
 
 		String[] route = headers.get("routing-key").split("\\.");
 		String service = route[0], command = route[1];
-		Map<String, Object> auth_res = null;
-//		if(!("login".equals(command) || "signup".equals(command))) {
-//			//authentication
-//			if(auth_res.get("error") != null) {
-//				servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-//				return auth_res;
-//			}
-//		}
-		Map<String, Object> res = (Map<String, Object>) amqpTemplate.convertSendAndReceive(
-				serviceToCommand.get(service), body, m -> {
-        	m.getMessageProperties().setHeader("command", command);
-//        	if(auth_res != null)
-//        		m.getMessageProperties().setHeader("user_id", (String)auth_res.get("data"));
-    		m.getMessageProperties().setHeader("user_id", "user_1");
-    		m.getMessageProperties().setReplyTo(RabbitMQConfig.reply_queue);
+		Map<String, Object> res = null;
+		//TODO: get signup command name from girls team
+		if(!("loginCommand".equals(command) || "signup".equals(command))) {
+			//authentication
+			Map<String, Object> auth_body = new HashMap<>();
+			auth_body.put("token", headers.get("token-x"));
+			final Map<String, Object> auth_res = (Map<String, Object>) amqpTemplate.convertSendAndReceive(
+					serviceToCommand.get("authentication"), auth_body, m -> {
+	        	m.getMessageProperties().setHeader("command", "verifyCommand");
+	    		m.getMessageProperties().setReplyTo(RabbitMQConfig.reply_queue);
+	        	return m;
+	        });
+			if(auth_res.get("error") != null) {
+				servletResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+				return auth_res;
+			}
+			res = (Map<String, Object>) amqpTemplate.convertSendAndReceive(
+					serviceToCommand.get(service), body, m -> {
+	        	m.getMessageProperties().setHeader("command", command);
+	        	m.getMessageProperties().setHeader("user_id", auth_res.get("data").toString());
+	    		m.getMessageProperties().setReplyTo(RabbitMQConfig.reply_queue);
 
-        	return m;
-        });
+	        	return m;
+	        });
+		}
+		else {
+			res = (Map<String, Object>) amqpTemplate.convertSendAndReceive(
+					serviceToCommand.get(service), body, m -> {
+	        	m.getMessageProperties().setHeader("command", command);
+	    		m.getMessageProperties().setReplyTo(RabbitMQConfig.reply_queue);
+
+	        	return m;
+	        });
+		}
+
 		if(res.get("error") != null) {
 			servletResponse.setStatus(HttpServletResponse.SC_BAD_GATEWAY);
 		}
@@ -65,3 +82,4 @@ public class Controller {
 	}
 
 }
+
