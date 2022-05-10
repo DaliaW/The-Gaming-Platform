cf7ed134273ffd8fc1e2128308169e6b27d56863
MonicaGeorge <monicageorge99@gmail.com>
2022-05-10 23:38:32 +0200

    added http server again

---
+++ http-server/src/main/java/http/amqp/RabbitMQConfig.java
@@ -0,0 +1,25 @@
+package http.amqp;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String reply_queue = "http_queue";
+
+
+    @Bean(name = {reply_queue})
+    public Queue reply_queue() {
+        return new Queue(reply_queue);
+    }
+
+    @Bean
+    public MessageConverter converter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+}
