db96ffa85ebc026daedd97b6b90943bf1dd83d1b
leqaaJamal <leqaa.abdulnasser@gmail.com>
2022-05-12 21:02:31 +0200

    added command classes

---
+++ user-service/src/main/java/guc/bttsBtngan/user/commands/UserPost/UnblockUserCommand.java
@@ -0,0 +1,15 @@
+package guc.bttsBtngan.user.commands.UserPost;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+
+@Component
+public class UnblockUserCommand extends UserPostCommand{
+
+    @Override
+    public Object execute(HashMap<String, Object> map) throws Exception {
+        return null;
+    }
+}
