830c3797f84242c7c9e80d69d58e64c1c8205b2e
leqaaJamal <leqaa.abdulnasser@gmail.com>
2022-05-12 22:29:15 +0200

    updated My commands in userPostController

--- user-service/src/main/java/guc/bttsBtngan/user/commands/UserPost/RecommendUserCommand.java
+++ user-service/src/main/java/guc/bttsBtngan/user/commands/UserPost/RecommendUserCommand.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
 @Component
-public class recommendUserCommand extends UserPostCommand{
+public class RecommendUserCommand extends UserPostCommand{
 
     @Override
     public Object execute(HashMap<String, Object> map) throws Exception {
