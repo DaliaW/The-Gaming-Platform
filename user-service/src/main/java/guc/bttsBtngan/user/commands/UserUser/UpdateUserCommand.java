63c0f1e2a7d75b77a46f151cb021a30f158872e1
mennaelnemr99 <49574177+mennaIbrahim99@users.noreply.github.com>
2022-05-15 17:34:05 +0200

    menmen and laqlouq
    working with and without commands

---
+++ user-service/src/main/java/guc/bttsBtngan/user/commands/UserUser/UpdateUserCommand.java
@@ -0,0 +1,15 @@
+package guc.bttsBtngan.user.commands.UserUser;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.HashMap;
+
+public class UpdateUserCommand extends UserUserCommand{
+
+    @Override
+    public Object execute(HashMap<String, Object> map) throws Exception {
+        return getService().updateUser((String)map.get("user_id"), (String)map.get("new_username"),
+                (String)map.get("new_email"),(String)map.get("oldPassword")
+                ,(String)map.get("newPassword"),(MultipartFile) map.get("photo"));
+    }
+}
---
+++ user-service/src/main/java/guc/bttsBtngan/user/data/UserUserInteraction.java
@@ -0,0 +1,115 @@
+package guc.bttsBtngan.user.data;
+
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+
+// For postgresql
+@Entity
+@Table
+public class UserUserInteraction {
+
+    @Id @GeneratedValue(generator="system-uuid")
+    @GenericGenerator(name="system-uuid", strategy = "uuid")
+    private String userId;
+    private String userName;
+    private String Password;
+    private String email;
+    private String photoRef;
+    private boolean isModerator;
+    private boolean isBanned;
+
+    public UserUserInteraction() {
+    }
+
+    public UserUserInteraction(String userId, String userName, String password, String email, String photoRef, boolean isModerator, boolean isBanned) {
+        super();
+        this.userId = userId;
+        this.userName = userName;
+        Password = password;
+        this.email = email;
+        this.photoRef = photoRef;
+        this.isModerator = isModerator;
+        this.isBanned = isBanned;
+    }
+
+    public UserUserInteraction(String userName, String password, String email, String photoRef, boolean isModerator, boolean isBanned) {
+        super();
+        this.userName = userName;
+        Password = password;
+        this.email = email;
+        this.photoRef = photoRef;
+        this.isModerator = isModerator;
+        this.isBanned = isBanned;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String password) {
+        Password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhotoRef() {
+        return photoRef;
+    }
+
+    public void setPhotoRef(String photoRef) {
+        this.photoRef = photoRef;
+    }
+
+    public boolean isModerator() {
+        return isModerator;
+    }
+
+    public void setModerator(boolean moderator) {
+        isModerator = moderator;
+    }
+
+    public boolean isBanned() {
+        return isBanned;
+    }
+
+    public void setBanned(boolean banned) {
+        isBanned = banned;
+    }
+
+    @Override
+    public String toString() {
+        return "UserUserInteraction{" +
+                "userId='" + userId + '\'' +
+                ", userName='" + userName + '\'' +
+                ", Password='" + Password + '\'' +
+                ", email='" + email + '\'' +
+                ", photoRef='" + photoRef + '\'' +
+                ", isModerator=" + isModerator +
+                ", isBanned=" + isBanned +
+                '}';
+    }
+
+}
