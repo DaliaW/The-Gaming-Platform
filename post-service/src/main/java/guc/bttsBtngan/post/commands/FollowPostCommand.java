1d91a5dda5a8854ea62603b8c33a936758a90ef4
Eiad Tantawy <eiadessam1@gmail.com>
2022-05-31 00:11:34 +0200

    user_id

--- post-service/src/main/java/guc/bttsBtngan/post/commands/FollowPostCommand.java
+++ post-service/src/main/java/guc/bttsBtngan/post/commands/FollowPostCommand.java
@@ -9,7 +9,7 @@ public class FollowPostCommand extends PostCommand{
 
     @Override
     public Object execute(HashMap<String, Object> map) throws Exception {
-    	return getService().followPost((String)map.get("userId"),
+    	return getService().followPost((String)map.get("user_id"),
         		(String)map.get("postId"),  
         		(boolean)map.get("follow"));
     }
